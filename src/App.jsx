eb558747dac01c0e909946ce2a45fcc52936cdb9
vagrant <mikefyke@hotmail.com>
2019-05-08 21:46:42 +0000

    added websocket allowed user to change name and relay message to server

--- src/App.jsx
+++ src/App.jsx
@@ -2,68 +2,59 @@ import React, {Component} from 'react';
 import ChatBar from './ChatBar.jsx';
 import MessageList from './MessageList.jsx';
 
-
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    // this.changeStateName = this.changeStateName.bind(this)
-    // this.addMessage = this.addMessage.bind(this)
     this.state = {
-    currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
-    messages: [
-      {
-        username: "Bob",
-        content: "Has anyone seen my marbles?",
-        id: 1
-      },
-      {
-        username: "Anonymous",
-        content: "No, I think you lost them. You lost your marbles Bob. You lost them for good.",
-        id: 2
-      }
-    ]
-  }
-  }
-  componentDidMount() {
-  console.log("componentDidMount <App />");
-  setTimeout(() => {
-    console.log("Simulating incoming message");
-    // Add a new message to the list of messages in the data store
-    const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
-    const messages = this.state.messages.concat(newMessage)
-    // Update the state of the app component.
-    // Calling setState will trigger a call to render() in App and all child components.
-    this.setState({messages: messages})
-  }, 3000);
-  }
-
-    addMessage = (message) => {
-      const newMessage = {
-      username: this.state.currentUser.name,
-      content: message,
-      id: this.state.messages.length + 1
+      currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
+      messages: []
     };
-    const messageState = this.state.messages.push(newMessage);
-    this.setState({messageState});
+    this.socket = new WebSocket('ws://localhost:3001');
   }
 
-
-  changeStateName(data){
-    this.setState ({currentUser: {name: data}})
+  componentDidMount() {
+    this.socket.onopen = () => {
+      console.log('Browser client connected');
+    };
+    this.socket.onmessage = (event) => {
+      let serverMessage = JSON.parse(event.data);
+      let updateMessage = [...this.state.messages, serverMessage]
+      this.setState({messages : updateMessage});
+    }
+    console.log("componentDidMount <App />");
   }
 
+  changeStateName = (info) => {
+   this.setState({ currentUser: { name: info } })
+  }
+
+  addMessage=(message)=>{
+    const newMessage ={
+      username: this.state.currentUser.name,
+      content:message,
+
+    }
+
+    this.socket.send(JSON.stringify(newMessage))
+
+
+    // this.setState({messageState});
+  }
 
   render() {
+
     return (
       <div>
         <nav className="navbar">
         <a href="/" className="navbar-brand">Chatty</a>
         </nav>
-        <ChatBar currentUser= {this.state.currentUser.name} addMessage={this.addMessage} />
-        <MessageList messages={this.state.messages}/>
+
+        <MessageList messages ={this.state.messages}/>
+        <ChatBar currentUser ={this.state.currentUser.name} addMessage={this.addMessage} changeStateName={this.changeStateName}/>
+
       </div>
     );
+
   }
 }
-
 export default App;
